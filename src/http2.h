c9fa5778e6c16e81054a1ee3976a1cbb628ad49b
Daniil Polovinkin <dolovinkin@gmail.com>
2016-02-17 14:56:59 +0300

    Added http2.h file with frame and frametype

---
+++ src/http2.h
@@ -0,0 +1,22 @@
+enum FrameType {
+	DATA = 0,
+	HEADERS = 1,
+	PRIORITY = 2,
+	RST_STREAM = 3,
+	SETTINGS = 4,
+	PUSH_PROMISE = 5,
+	PING = 6,
+	GOAWAY = 7,
+	WINDOW_UPDATE = 8
+};
+
+struct Http2Frame {
+	unsigned int length: 24;
+	enum FrameType type;
+	unsigned char flags;
+	unsigned char reserved: 1;
+	unsigned long int streamIdentifier: 31;
+	char * payload;
+};
+
+void send_frame(struct Http2Frame *frame, int sockfd);
